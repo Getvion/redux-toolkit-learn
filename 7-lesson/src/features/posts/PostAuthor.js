2212b25dc8c274c55d28ca35634a9ea9a579a2b2
Oleg Somov <gog0125@mail.ru>
2022-08-17 13:26:33 +0300

    feat: add 7 lesson

---
+++ 7-lesson/src/features/posts/PostAuthor.js
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectAllUsers } from '../users/usersSlice';
+
+const PostAuthor = ({ userId }) => {
+  const users = useSelector(selectAllUsers);
+
+  const author = users.find((user) => user.id === userId);
+
+  return <span>by {author ? <Link to={`/user/${userId}`}>{author.name}</Link> : 'unknown author'}</span>;
+};
+export default PostAuthor;
